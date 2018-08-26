6afdca301462bf8f53534826a1487211755638fd
liang <zh_l2006@sina.com>
2018-08-26 10:33:42 -0600

    merge master

--- src/app.js
+++ src/app.js
@@ -3,24 +3,4 @@ import ReactDOM from 'react-dom';
 import CastIronStore from './store/CastIronStore';
 import Dashboard from './view/Dashboard';
 
-<<<<<<< HEAD
-// const routes = (
-//     <BrowserRouter>
-//         <div>
-//             <Switch>
-//                 <Route path="/receipts" component={ReceiptsView} />
-//                 <Route path="/" component={Dashboard}  />
-//                 <Route component={NotFound} />
-
-//             </Switch>
-//         </div>
-//     </BrowserRouter>
-// )
-
-
-
-ReactDOM.render(<Dashboard/>, document.getElementById('app'));
-
-=======
 ReactDOM.render(<Dashboard />, document.getElementById('app'));
->>>>>>> master
