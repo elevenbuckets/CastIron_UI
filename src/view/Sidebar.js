50bace7c52ab7c4648c2a8c40017b8bc4f565dad
Yu-Cheng Lin <infwonder@gmail.com>
2018-09-20 20:30:06 -0600

    fix Receipts button bug when log out, clean up unused functions

--- src/view/Sidebar.js
+++ src/view/Sidebar.js
@@ -33,40 +33,6 @@ class Sidebar extends Reflux.Component {
 
   }
 
-  handleClick = () => { this.toggleSettings(); }
-
-  isCustomGasPriceValid = () => {
-    return (this.state.gasPriceOption != "custom" || this.state.customGasPrice)
-  }
-
-  toggleSettings = () => {
-    this.setState({ visible: !this.state.visible });
-  }
-
-  handleChange = (event) => {
-    CastIronActions.startUpdate(event.value, this.refs.canvas);
-  }
-
-  handleToggle = (event) => {
-    let pt = !this.state.ptoggle;
-    let sb = pt ? 'none' : 'inline-block';
-    let pf = pt ? '100px' : '283px';
-    this.setState({ ptoggle: pt, pfield: pf, sbutton: sb });
-    CastIronActions.masterUpdate(this.refs.mp.value);
-  }
-
-  handleGasPriceSelect = (event) => {
-    CastIronActions.gasPriceOptionSelect(event.currentTarget.defaultValue);
-  }
-
-  handleCustomGasPriceUpdate = (price) => {
-    CastIronActions.customGasPriceUpdate(price);
-  }
-
-  handleEnter = (event) => {
-    if (event.keyCode === 13) CastIronActions.masterUpdate(this.refs.mp.value);
-  }
-
   handleLogout = () => {
     CastIronActions.masterUpdate('');
   }
@@ -84,6 +50,20 @@ class Sidebar extends Reflux.Component {
     }
   }
 
+  handleReceiptClick = () => {
+    console.log("in handleReceiptClick");
+    if (this.state.stoggle === false) {
+      console.log(`switch from ${this.state.currentView} to Receipts`);
+      this.setState({ sbefore: this.state.currentView, stoggle: true });
+      CastIronActions.changeView("Receipts");
+    } else {
+      console.log(`switch from Receipts back to previous view`);
+      CastIronActions.changeView(this.state.sbefore);
+      this.setState({ stoggle: false, sbefore: 'Settings' });
+    }
+  }
+
+
   copyAddress = () => {
     var dummy = document.createElement("input");
     document.body.appendChild(dummy);
@@ -92,20 +72,6 @@ class Sidebar extends Reflux.Component {
     dummy.select();
     document.execCommand("copy");
     document.body.removeChild(dummy);
-  }
-
-  // Questionable design.. Need reviews
-  handleReceiptClick = () => {
-    console.log("in handleReceiptClick");
-    if (this.state.rtoggle === 'Receipts') {
-      console.log(`switch from ${this.state.currentView} to Receipts`);
-      this.setState({ rtoggle: this.state.currentView });
-      CastIronActions.changeView("Receipts");
-    } else {
-      console.log(`switch from Receipts back to previous view`);
-      CastIronActions.changeView(this.state.rtoggle);
-      this.setState({ rtoggle: 'Receipts' });
-    }
   }
 
   handleSettingClick = () => {
@@ -157,7 +123,7 @@ class Sidebar extends Reflux.Component {
         <input type="button" className="button sbutton logout" value="Log Out" style={{ display: this.state.stoggle ? 'none' : true }} onClick={this.handleLogout} />
         <input type="button" className={this.state.stoggle ? "button sbutton logout" : "button sbutton settings"} value={this.state.stoggle ? 'Back' : 'Settings'} onClick={this.handleSettingClick} />
         <input type="button" className="button sbutton drawer" value="Apps" style={{ display: this.state.stoggle ? 'none' : true }} onClick={this.handleDrawerClick} />
-        <input type="button" className="button sbutton receipts" value={this.state.rtoggle} style={{ display: this.state.stoggle ? 'none' : true, animation: n > 0 ? "bgColor 1.1s infinite alternate" : "none" }} onClick={this.handleReceiptClick} />
+        <input type="button" className="button sbutton receipts" value='Receipts' style={{ display: this.state.stoggle ? 'none' : true, animation: n > 0 ? "bgColor 1.1s infinite alternate" : "none" }} onClick={this.handleReceiptClick} />
       </div>
     );
   }
