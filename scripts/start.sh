8d405d69f0d417db97ad28b9484d2cbb53a45a49
Yu-Cheng Lin <infwonder@gmail.com>
2018-09-28 15:44:09 -0600

    fix small issues during distRelease

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,50 @@
-start_rinkeby.sh
+#!/bin/bash
+
+CWD=`pwd`;
+LOCALDIR="$CWD/linux-unpacked/public/.local";
+DATADIR=`dirname $(dirname $CWD)`
+ANS=`jq .configDir $LOCALDIR/bootstrap_config.json`;
+
+# let nothing be really nothing
+if [ $ANS == '""' ]; then
+       	ANS='';
+	echo "Initialization expected, auto container termination disabled.";
+fi
+
+function blocking() {
+	NewANS=`jq .configDir $LOCALDIR/bootstrap_config.json`;
+	[ "$ANS" != "$NewANS" ] && holding || shutdown;
+}
+
+function holding() {
+	echo "Please press \"Enter\" to terminate geth container ..."
+	read ANS;
+	shutdown;
+}
+
+function shutdown() { 
+	echo "Shutting down, please wait ...";
+	docker stop geth_stunnel_rinkeby && exit 0; 
+}
+
+
+function dockerIP() { 
+	IP=`docker inspect geth_stunnel_rinkeby|grep -w IPAddress -m1|grep -oE [0-9.]+`;
+	ssh -L 8545:localhost:8545 -N -f -l eleven ${IP};
+}
+
+function dockerInit() {
+	docker start geth_stunnel_rinkeby 2> /dev/null || \
+	docker run --name geth_stunnel_rinkeby -p 30303:30303 -p 30303:30303/udp -p 30304:30304/udp -td -v"${DATADIR}:/data:z" geth_stunnel_rinkeby
+}
+
+function startUI() {
+	cd ./linux-unpacked && \
+	./cast-iron-app
+}
+
+# Main
+dockerInit && \
+dockerIP && \
+startUI && \
+blocking;
